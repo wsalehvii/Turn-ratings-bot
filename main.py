bd0c7d09a521cb899ada31b1cd166e4abf308012
CisCo_X <Shadow.dsg11@gmail.com>
2024-04-01 22:15:02 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def add_numbers(a, b):
     return a + b
   
-  def test_add_numbers():
+def test_add_numbers():
     assert add_numbers(2, 3) == 5
     assert add_numbers(0, 0) == 0
     assert add_numbers(-1, 1) == 0
