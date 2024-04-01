e5bcd441452b67b29ce20685a97edff69711447e
CisCo_X <Shadow.dsg11@gmail.com>
2024-04-01 22:18:01 +0330

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,11 +4,4 @@ def add_numbers(a, b):
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
     assert add_numbers(0, 0) == 0
-    assert add_numbers(-1, 1) == 0
-    
-def main():
-  print("hello world")
-
-
-if __name__ == "__main__":
-    main()
+    assert add_numbers(-1, 1) == 1
